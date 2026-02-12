02c7ff66696140e2b80822f1c851b802d7192c8b
marianaa42 <mariana05aguilar@gmail.com>
2026-02-11 19:33:19 -0800

    Lab 1

--- addition-starter-lab/firmware/src/asmFunc.s
+++ addition-starter-lab/firmware/src/asmFunc.s
@@ -9,7 +9,7 @@
 /* create a string */
     
 /*** STUDENTS: HINT: this is the string that the C code prints out!  **/
-nameStr: .asciz "Hello. My name is Inigo Montoya."  
+nameStr: .asciz "Hello. My name is Mariana Aguilar."  
 
  .align
  
@@ -53,7 +53,7 @@ asmFunc:
     push {r4-r11,LR}
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
-
+    ADD R0, R0, R1
 
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
     
